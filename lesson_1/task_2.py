4e96af46a0a118ae64d5d5735fcfdfacea23f03a
Anton Tynnikov <tynnikov283@gmail.com>
2020-11-19 19:36:58 +0300

    Make task_1 and task_2

---
+++ lesson_1/task_2.py
@@ -0,0 +1,10 @@
+# По введенным пользователем координатам двух точек вывести уравнение прямой вида y = kx + b, проходящей через эти точки
+x1 = int(input("Enter integer: "))
+x2 = int(input("Enter integer: "))
+y1 = int(input("Enter integer: "))
+y2 = int(input("Enter integer: "))
+
+k = (y1 - y2) / (x1 - x2)
+b = y2 - k * x2
+
+print(k,b)
